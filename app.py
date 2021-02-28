d0f8bba44636aa34354949adfbe9769b0c55ebdb
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-02-27 23:10:56 -0500

    moved a file to templates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-# import scrape_mars
-from scrape_mars import scrape_all
+import scrape_mars
+# from scrape_mars import scrape_all
 import time
 
 app = Flask(__name__)
@@ -9,13 +9,13 @@ app = Flask(__name__)
 app.config["MONGO_URI"]="mongodb://localhost:27017/mars_app"
 mongo=PyMongo(app)
 
-@app.route("/")
+@app.route('/')
 def home():
 # List all available api routes.
     mars=mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
-@app.route("/scrape")
+@app.route('/scrape')
 def scrape():
     mars=mongo.db.mars
     mars_data=scrape_mars.scrape_all()
