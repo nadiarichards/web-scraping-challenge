1c7150b6410427f44077f4a2e15f6f8c4efd1d12
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-02-28 00:06:44 -0500

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import time
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"]="mongodb://localhost:27017/mars_app"
-mongo=PyMongo(app)
+# app.config["MONGO_URI"]="mongodb://localhost:27017/mars_app"
+# mongo=PyMongo(app)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route('/')
 def home():
